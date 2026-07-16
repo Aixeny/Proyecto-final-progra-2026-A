b383235b38034080bc88460e5585c578da318f29
Aixeny <joelandres2809@gmail.com>
2026-07-15 19:04:23 -0500

    Cambio de interfaces con botones y nombres diferenciales entre objetos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btn_registrar_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(ui->stackedWidget->indexOf(ui->page_Registrar));
+}
+
+
+void MainWindow::on_btn_ver_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(ui->stackedWidget->indexOf(ui->page_Ver));
+}
+
+
+void MainWindow::on_btn_actualizar_clicked()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,11 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+private slots:
+    void on_btn_registrar_clicked();
+
+    void on_btn_ver_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
